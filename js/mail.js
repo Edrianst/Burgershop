aefedd03664f0d4281b4e88dbb596452362d40e3
Denis Kirsanov <excite90@mail.ru>
2017-11-26 19:30:28 +0300

    added ajax form and modal for it. Hopefully last commit)

--- js/mail.js
+++ js/mail.js
@@ -1,17 +1,39 @@
-    const form = $('#order-form');
+var submitForm = function (ev) {
+    ev.preventDefault();
 
-    form.on('submit', e => {
-        e.preventDefault();
+    var form = $(ev.target);
+        
+    var request = ajaxForm(form);
 
-        });
-        const request = $.ajax({
-            url: form.attr('action'),
-            method: "POST",
-            data: {
-                id: form
-            },
-            dataType: "json"
-        });
-        request.fail(function (jqHR, textStatus) {
-            $.fancybox.open('<div id="hidden-form"><div class="modal__text">Сообщение отправлено</div><a href="#" class="order-btn">Закрыть</a></div>');
-        })
+    request.done(function(msg) {
+        var mes = msg.mes,
+            status = msg.status;
+        if (status === 'OK') {
+            $.fancybox.open('<div id="hidden-form"><div class="modal__text">Заявка была успешно отправлена</div><a href="#" data-fancybox-close class="order-btn">Закрыть</a></div>');
+        } else{
+            $.fancybox.open('<div id="hidden-form"><div class="modal__text">К сожалению, письмо не было отправлено, попробуйте позже или свяжитесь с нами по телефону!</div><a href="#" data-fancybox-close class="order-btn">Закрыть</a></div>');
+        };
+        $('#order-form')[0].reset();form.reset();
+    });
+
+    request.fail(function(jqXHR, textStatus) {
+        // alert("Request failed: " + textStatus);
+        $.fancybox.open('<div id="hidden-form"><div class="modal__text">К сожалению, письмо не было отправлено, попробуйте позже или свяжитесь с нами по телефону!</div><a href="#" data-fancybox-close class="order-btn">Закрыть</a></div>');
+    });
+}
+
+var ajaxForm = function (form) {
+
+    var url = form.attr('action'),
+        data = form.serialize();
+
+    return $.ajax({
+        type: 'POST',
+        url: url,
+        data: data,
+        dataType: 'JSON'
+    });
+
+}
+
+$('#order-form').on('submit', submitForm);
--- mail.php
+++ mail.php
@@ -1,11 +1,18 @@
 
 <?php
-    $name = $_POST['user-name'];
-    $email = $_POST['user-email'];
-    $pay = $_POST['pay-option'];
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $phone = $_POST['phone'];
+    $street = $_POST['street'];
+    $house = $_POST['house'];
+    $corp = $_POST['corp'];
+    $flat = $_POST['flat'];
+    $floor = $_POST['floor'];
+    $cashchange = $_POST['cashchange'];
+    $card = $_POST['card'];
     $message = $_POST['message'];
-    $disturb = $_POST['dont-disturb']; // 1 или null 
-    $disturb = isset($disturb) ? 'НЕТ' : 'ДА';
+    $callback = $_POST['callback']; // 1 или null 
+    $callback = isset($callback) ? 'НЕТ' : 'ДА';
 
     $mail_message = '
     <html>
@@ -17,15 +24,22 @@
             <ul>
                 <li>Имя: ' . $name . '</li>
                 <li>Email: ' . $email . '</li>
-                <li>Способ оплаты: ' . $pay . '</li>
+                <li>Телефон: ' . $phone . '</li>
+                <li>Улица: ' . $street . '</li>
+                <li>Дом: ' . $house . '</li>
+                <li>Корпус: ' . $corp . '</li>
+                <li>Квартира: ' . $flat . '</li>
+                <li>Этаж: ' . $floor . '</li>
+                <li>Сдача: ' . $cashchange . '</li>
+                <li>Способ оплаты: ' . $card . '</li>
                 <li>Комментарии к заказу: ' . $message . '</li>
-                <li>Нужно ли перезванивать клиенту: ' . $disturb . '</li>
+                <li>Нужно ли перезванивать клиенту: ' . $callback . '</li>
             </ul>
         </body>
     </html>    
     ';
 
-    $headers = "From: Администратор сайта <admin@loftschool.com>\r\n".
+    $headers = "From: Администратор сайта <mr.burger@burgers.com>\r\n".
     "MIME-Version: 1.0" . "\r\n" .
     "Content-type: text/html; charset=UTF-8" . "\r\n";
     $mail = mail('excite90@mail.ru', 'Заказ', $mail_message, $headers);
